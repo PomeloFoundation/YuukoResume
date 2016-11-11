4247fb4743da22a3ed7c502cf7e153bc14f55e40
雨宮丶優子 <911574351@qq.com>
2016-11-11 16:47:25 +0800

    Implement Education, Skill, Certificate management logics

--- src/YuukoResume/Controllers/AdminController.cs
+++ src/YuukoResume/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using YuukoResume.Models;
 using YuukoResume.Filters;
 
@@ -68,6 +69,7 @@ namespace YuukoResume.Controllers
         public IActionResult Profile(Profile Model)
         {
             Startup.Profile = Model;
+            System.IO.File.WriteAllText("profile.json", JsonConvert.SerializeObject(Model));
             return Prompt(x => 
             {
                 x.Title = SR["Succeeded"];
@@ -108,17 +110,18 @@ namespace YuukoResume.Controllers
         [Route("[controller]/Project/Edit/{id}")]
         public async Task<IActionResult> EditProject(long id, Project Model)
         {
-            var project = await DB.Projects.SingleAsync(x => x.Id == id);
-            project.BlobId = Model.BlobId;
-            project.Catalog = Model.Catalog;
-            project.DemoUrl = Model.DemoUrl;
-            project.Description = Model.Description;
-            project.From = Model.From;
-            project.To = Model.To;
-            project.GitHub = Model.GitHub;
-            project.Tags = Model.Tags;
-            project.Title = Model.Title;
-            await DB.SaveChangesAsync();
+            await DB.Projects
+                .Where(x => x.Id == id)
+                .SetField(x => x.BlobId).WithValue(Model.BlobId)
+                .SetField(x => x.Catalog).WithValue(Model.Catalog)
+                .SetField(x => x.DemoUrl).WithValue(Model.DemoUrl)
+                .SetField(x => x.Description).WithValue(Model.Description)
+                .SetField(x => x.From).WithValue(Model.From)
+                .SetField(x => x.To).WithValue(Model.To)
+                .SetField(x => x.GitHub).WithValue(Model.GitHub)
+                .SetField(x => x.Tags).WithValue(Model.Tags)
+                .SetField(x => x.Title).WithValue(Model.Title)
+                .UpdateAsync();
             return Prompt(x =>
             {
                 x.Title = SR["Succeeded"];
@@ -145,6 +148,138 @@ namespace YuukoResume.Controllers
                 x.Details = SR["Project info has been created successfully."];
             });
         }
+        #endregion
+
+        #region Education Management
+        [HttpGet]
+        [AdminRequired]
+        public async Task<IActionResult> Education() => View(await DB.Educations.OrderByDescending(x => x.From).ThenBy(x => x.To).ToListAsync());
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Education/Create")]
+        public async Task<IActionResult> CreateEducation(Education Model)
+        {
+            DB.Educations.Add(Model);
+            await DB.SaveChangesAsync();
+            return Prompt(x =>
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Education information has been created successfully."];
+            });
+        }
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Education/Remove/{id}")]
+        public async Task<IActionResult> RemoveEducation(long id)
+        {
+            await DB.Educations
+                .Where(x => x.Id == id)
+                .DeleteAsync();
+            return Prompt(x =>
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Education information has been removed successfully."];
+            });
+        }
+        #endregion
+
+        #region Skill Management
+        [HttpGet]
+        [AdminRequired]
+        public async Task<IActionResult> Skill() => View(await DB.Skills.GroupBy(x => x.Performance).Select(x => new { Performance = x.Key, Skills = x.OrderByDescending(y => y.Level) }).ToListAsync());
+        
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Skill/Create")]
+        public async Task<IActionResult> CreateSkill(Skill Model)
+        {
+            DB.Skills.Add(Model);
+            await DB.SaveChangesAsync();
+            return Prompt(x =>
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Skill has been created successfully."];
+            });
+        }
+
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Skill/Remove/{id}")]
+        public async Task<IActionResult> RemoveSkill(long id)
+        {
+            await DB.Skills
+                .Where(x => x.Id == id)
+                .DeleteAsync();
+            return Prompt(x =>
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Skill has been removed successfully."];
+            });
+        }
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Skill/Edit/{id}")]
+        public async Task<IActionResult> EditSkill(long id, float level)
+        {
+            await DB.Skills
+                .Where(x => x.Id == id)
+                .SetField(x => x.Level).WithValue(level)
+                .UpdateAsync();
+            return Prompt(x => 
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Skill has been saved successfully."];
+            });
+        }
+        #endregion
+
+        #region Certificate Management
+        [HttpGet]
+        [AdminRequired]
+        public async Task<IActionResult> Certificate() => View(await DB.Certificates.OrderByDescending(x => x.PRI).ToListAsync());
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Certificate/Create")]
+        public async Task<IActionResult> CreateCertificate(Certificate Model)
+        {
+            DB.Certificates.Add(Model);
+            await DB.SaveChangesAsync();
+            return Prompt(x => 
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Certificate has been created successfully."];
+            });
+        }
+
+        [HttpPost]
+        [AdminRequired]
+        [ValidateAntiForgeryToken]
+        [Route("[controller]/Certificate/Remove/{id}")]
+        public async Task<IActionResult> RemoveCertificate(long id)
+        {
+            await DB.Certificates
+                .Where(x => x.Id == id)
+                .DeleteAsync();
+            return Prompt(x =>
+            {
+                x.Title = SR["Succeeded"];
+                x.Details = SR["Certificate has been removed successfully."];
+            });
+        }
+        #endregion
+
+        #region Experience Management
         #endregion
     }
 }
--- src/YuukoResume/Models/Certificate.cs
+++ src/YuukoResume/Models/Certificate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Pomelo.AspNetCore.Extensions.BlobStorage.Models;
 
@@ -7,6 +8,10 @@ namespace YuukoResume.Models
     public class Certificate
     {
         public long Id { get; set; }
+
+        [Localized]
+        [MaxLength(512)]
+        public string Title { get; set; }
 
         [ForeignKey("Blob")]
         public Guid BlobId { get; set; }
