858bcc1c8bd97720497e822c49b413fb8753c195
Tory Xu <toryxu@live.cn>
2016-11-17 09:54:12 +0800

    .NET Core 1.1.0 & Azure App Service supported
    Upgraded to .NET Core 1.1.0
    Can run as App Service on Azure, including Azure Global & China

--- src/YuukoResume/Program.cs
+++ src/YuukoResume/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace YuukoResume
@@ -13,8 +9,8 @@ namespace YuukoResume
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://*:80")
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
